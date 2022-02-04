b430ccf95652bb79f74ca1b27a4de9aaa44667a9
Marco Vavassori <marcovava2001@gmail.com>
2022-02-04 15:30:13 +0000

    Check that Main and Board can communicate

--- ADS_cw_40504087/Coursework/Main.py
+++ ADS_cw_40504087/Coursework/Main.py
@@ -3,6 +3,8 @@ This file is the main file for the Sudoku game
 @author Marco Vavassori
 @date 04/02/2022
 '''
+from Board import Board
 
 # Main
-print("Just testing")
+sudokuBoard: Board = Board(3, 4)
+print(str(sudokuBoard.getWidth()))
